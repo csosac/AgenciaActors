e25347db84015f309af4bee099441740d029a1d0
Barchar <csosac@gmail.com>
2017-02-16 16:19:38 +0100

    backup de bdd

--- BACKUPS BBDD/agenciacristianaitor.sql
+++ BACKUPS BBDD/agenciacristianaitor.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-02-2017 a las 16:26:38
+-- Tiempo de generación: 16-02-2017 a las 16:18:47
 -- Versión del servidor: 10.1.19-MariaDB
--- Versión de PHP: 5.6.28
+-- Versión de PHP: 7.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -48,6 +48,13 @@ CREATE TABLE `director` (
   `lastname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `director`
+--
+
+INSERT INTO `director` (`id`, `nif`, `name`, `lastname`) VALUES
+(1, '47807197H', 'David', 'Yates');
+
 -- --------------------------------------------------------
 
 --
@@ -56,13 +63,21 @@ CREATE TABLE `director` (
 
 CREATE TABLE `film` (
   `id` int(11) NOT NULL,
-  `idFilm` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `type` varchar(70) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `startDate` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `endDate` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+  `startDate` date DEFAULT NULL,
+  `endDate` date DEFAULT NULL,
+  `photoURL` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `directorId` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `film`
+--
+
+INSERT INTO `film` (`id`, `name`, `description`, `type`, `startDate`, `endDate`, `photoURL`, `directorId`) VALUES
+(1, 'Harry Potter y la piedra filosofal', 'Harry Potter y la piedra filosofal es una película de fantasía y aventuras basada en el libro homónimo de J. K. Rowling, dirigida por el cineasta Chris Columbus y estrenada en 2001', 'ciencia ficcion', '2012-01-01', '2012-01-02', 'http://3.bp.blogspot.com/-TF8bS9iO95Q/Tdhgtn3MXTI/AAAAAAAAAi4/66xBC_2W1QU/s275/harry-potter-y-la-piedra-filosofal.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -72,10 +87,30 @@ CREATE TABLE `film` (
 
 CREATE TABLE `role` (
   `id` int(11) NOT NULL,
-  `nifActor` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `idFilm` int(11) DEFAULT NULL,
-  `role` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL
+  `role` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `actorId` int(11) DEFAULT NULL,
+  `filmId` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `role` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `role`) VALUES
+(1, 'barchar', 'iOgyhdY1gJJPj7y7mMN8obgqMQZH2fLDuQuXfqZesC1Iqxo6iHxRuAA9m8E1ZUz76OIiPGTann7uJ3BNhPDoEA==', NULL);
 
 --
 -- Índices para tablas volcadas
@@ -100,12 +135,20 @@ ALTER TABLE `director`
 --
 ALTER TABLE `film`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `UNIQ_8244BE22F1B9E0BC` (`idFilm`);
+  ADD KEY `IDX_8244BE2221DB3FB1` (`directorId`);
 
 --
 -- Indices de la tabla `role`
 --
 ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_57698A6A6DCBA9B2` (`actorId`),
+  ADD KEY `IDX_57698A6AA1D6191D` (`filmId`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -121,17 +164,39 @@ ALTER TABLE `actor`
 -- AUTO_INCREMENT de la tabla `director`
 --
 ALTER TABLE `director`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT de la tabla `film`
 --
 ALTER TABLE `film`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT de la tabla `role`
 --
 ALTER TABLE `role`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `film`
+--
+ALTER TABLE `film`
+  ADD CONSTRAINT `FK_8244BE2221DB3FB1` FOREIGN KEY (`directorId`) REFERENCES `director` (`id`);
+
+--
+-- Filtros para la tabla `role`
+--
+ALTER TABLE `role`
+  ADD CONSTRAINT `FK_57698A6A6DCBA9B2` FOREIGN KEY (`actorId`) REFERENCES `actor` (`id`),
+  ADD CONSTRAINT `FK_57698A6AA1D6191D` FOREIGN KEY (`filmId`) REFERENCES `film` (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
